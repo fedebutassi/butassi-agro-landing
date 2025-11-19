ddc1c03aeb7d49a3e57412e00730f7c797cc889b
federicobutassi <federicobuta51@gmail.com>
2025-11-19 10:19:54 -0300

    actualizacion pizarra.

--- src/pages/Pizarra.tsx
+++ src/pages/Pizarra.tsx
@@ -25,7 +25,7 @@ const Pizarra = () => {
                 {/* Imagen de pizarra */}
                 <div className="w-full aspect-[9/16] bg-muted rounded-lg overflow-hidden">
                   <img
-                    src="/pizarra1811.png"
+                    src="/pizarra1911.png"
                     alt="Pizarra de Rosario"
                     className="w-full h-full object-cover"
                   />
